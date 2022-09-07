781091698fd31d18974994b2d4421a1d51cdf712
RenLem <ag.tropico@gmail.com>
2022-09-07 22:59:21 +0200

    Primordial

---
+++ app/src/main/java/by/renatopuskaric/myapplication/Card.kt
@@ -0,0 +1,12 @@
+package by.renatopuskaric.myapplication
+
+import android.content.Context
+import android.icu.text.CaseMap
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+
+class Card @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+}
